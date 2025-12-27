825b4c210984089eaa71922c525f1c55ed4971ab
AtesiT <atesit@mail.ru>
2025-12-27 11:01:31 +0300

    ADD: TableViewController in Storyboard

---
+++ randomUserAPIApp/ViewControllers/TableViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
--- randomUserAPIApp/ViewControllers/ViewController.swift
+++ randomUserAPIApp/ViewControllers/ViewController.swift
@@ -1,12 +1,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedTableVCButton(_ sender: Any) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navVC = segue.destination as? UINavigationController
+        let theVC2 = navVC?.topViewController as? TableViewController
+    }
 }
 
